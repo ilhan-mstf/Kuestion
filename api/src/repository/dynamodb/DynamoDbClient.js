502e92331d315117a25d43f192304c9142d90a91
Mustafa İlhan <ilhan.mstf@gmail.com>
2020-05-26 10:55:08 +0300

    Set variables to yml

--- api/src/repository/dynamodb/DynamoDbClient.js
+++ api/src/repository/dynamodb/DynamoDbClient.js
@@ -1,9 +1,11 @@
 const AWS = require('aws-sdk')
 
-AWS.config.update({
-  region: "local",
-  endpoint: "http://localhost:8000"
-})
+if (process.env.IS_OFFLINE) {
+  AWS.config.update({
+    region: process.env.REGION,
+    endpoint: process.env.DYNAMODB_ENDPOINT
+  })
+}
 
 const docClient = new AWS.DynamoDB.DocumentClient()
 
